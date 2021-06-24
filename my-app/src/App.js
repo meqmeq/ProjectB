cb23db81ff167945953cf0ca2ae53cb15bf68d8a
Marc <mecquilatan@gmail.com>
2021-06-23 22:02:49 -0400

    Added a barebone score counter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,24 @@
-function App() {
+import React, { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [score, updateScore] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">Why hello best baeyo</header>
+      <h1 className="App-header">Why hello best baeyo</h1>
+      <div>
+        score: {score}
+        <button
+          onClick={() => {
+            updateScore(score + 1);
+          }}
+        >
+          add
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
